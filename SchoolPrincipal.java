0a666122caf8766e5471d1847cb20d464775556b
Animegh9365 <animeghsarania9365@gmail.com>
2024-09-17 22:33:45 +0530

    Singleton Design Pattern

---
+++ SchoolPrincipal.java
@@ -0,0 +1,18 @@
+public class SchoolPrincipal {
+    private static SchoolPrincipal principal;
+
+    private SchoolPrincipal() {
+        System.out.println("A new principal has been appointed");
+    }
+
+    public static SchoolPrincipal getInstance() {
+        if(principal == null) {
+            principal = new SchoolPrincipal(); 
+        }
+        return principal;
+    }
+
+    public void makeAnnouncement() {
+        System.out.println("The pricipal is making an announcement.");
+    }
+}
